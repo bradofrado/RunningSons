64ffd56d8ec1b1bb087b83e2b2680d5ee837e1d7
Braydon Jones <bradofrado@gmail.com>
2022-05-16 23:01:47 -0600

    Added vue and running sons home page

---
+++ front-end/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+body {
+    background-image: linear-gradient(-180deg, #4a6d6c , #033740);
+    background-size: 200% 200%;
+    animation: gradient 5s ease infinite;
+    margin: 0px;
+}
+
+@keyframes gradient {
+    0%{background-position:10% 0%}
+    50%{background-position:91% 100%}
+    100%{background-position:10% 0%}
+}
+#app {
+  font-family: 'Montserrat', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fff;
+  height: 100vh;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ front-end/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="home-container">
+        <h1 class="title">Running Sons</h1>
+        <p>Music and Merchandise coming soon...</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+.title {
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 5em;
+    text-shadow: 2px 4px 20px #283b3b;
+}
+
+.home-container {
+    padding-top: 150px;
+}
+</style>
