ecf63baa9a4093e71e5b555e65ca7fce0db54829
Braydon Jones <bradofrado@gmail.com>
2022-07-28 08:43:10 -0600

    Made the header with cart and profile icon look better for mobile

--- front-end/src/components/Header.vue
+++ front-end/src/components/Header.vue
@@ -2,14 +2,34 @@
     <b-navbar class="bg-primary-dark" toggleable="lg" type="dark">
         <div class="container header-container">
             <b-navbar-brand href="/">Running Sons</b-navbar-brand>
-
+            <b-navbar-nav class="mobile navbar-singleline b-width-expand">
+                <b-nav-item to="/cart" active-class="active" class="cart-icon">
+                    <icon class="active-circle" icon="cart" circle v-num-icon="numCartItems"/>
+                </b-nav-item>
+                <b-nav-item v-if="user" to="/account" exact-active-class="active">
+                    <icon class="active-circle" icon="profile" circle/>
+                </b-nav-item>
+            </b-navbar-nav>
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
             <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav>
+                <b-navbar-nav >
                     <b-nav-item to="/" exact-active-class="active">Home</b-nav-item>
                     <b-nav-item to="/music" active-class="active">Music</b-nav-item> 
                     <b-nav-item id="merch-popover" active-class="active" to="/merchandise" @click="onMerchClick">Merchandise</b-nav-item>                                                        
+                </b-navbar-nav>
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-item to="/cart" active-class="active" class="cart-icon not-mobile">
+                        <icon class="active-circle" icon="cart" circle v-num-icon="numCartItems"/>
+                    </b-nav-item>
+                    <b-nav-item v-if="user" to="/account" exact-active-class="active" class="not-mobile">
+                        <icon class="active-circle" icon="profile" circle/>
+                    </b-nav-item>
+                    <template v-else>
+                        <b-nav-item  to="/account/signup" active-class="active">Sign up</b-nav-item>
+                        <span class="not-mobile">|</span>
+                        <b-nav-item  to="/account/login" active-class="active">Login</b-nav-item>
+                    </template>
                 </b-navbar-nav>
                 <b-popover v-if="types" ref="popover" target="merch-popover" triggers="hover blur" placement="bottom">
                         <b-navbar-nav>
@@ -17,20 +37,6 @@
                         </b-navbar-nav>
                 </b-popover>   
             </b-collapse>
-            <ul class="navbar-nav navbar-singleline">
-                <b-nav-item to="/cart" active-class="active" class="cart-icon">
-                    <icon class="active-circle" icon="cart" circle v-num-icon="numCartItems"/>
-                </b-nav-item>
-                <b-nav-item v-if="user" to="/account" exact-active-class="active">
-                    <icon class="active-circle" icon="profile" circle/>
-                </b-nav-item>
-                <template v-else>
-                    <b-nav-item  to="/account/signup" active-class="active">Sign up</b-nav-item>
-                    |
-                    <b-nav-item  to="/account/login" active-class="active">Login</b-nav-item>
-                    
-                </template>
-            </ul>
         </div>
     </b-navbar>
 </template>
@@ -76,8 +82,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.not-mobile {
+    display: none;
+}
+
+.mobile {
+    display: inherit;
+}
+
 .collapsed {
     order: 2
+}
+
+.navbar-nav {
+    align-items: center;
 }
 
 .nav-dropdown-item {
@@ -107,5 +125,19 @@ export default {
 }
 .cart-icon {
     position: relative;
+}
+
+.b-width-expand {
+    flex: 1;
+}
+
+@media only screen and (min-width: 960px) {
+    .not-mobile {
+        display: inherit;
+    }
+
+    .mobile {
+        display: none;
+    }
 }
 </style>
