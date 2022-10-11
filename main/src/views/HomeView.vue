5310434ef0584985c417b1e6e5dce4fbedc51987
Braydon Jones <bradofrado@gmail.com>
2022-10-10 22:32:05 -0600

    Changed title text for runningsons.com

--- main/src/views/HomeView.vue
+++ main/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="title-container">
         <h1 class="title">Running Sons</h1>
-        <p class="sub-title">Album <em>Falling Sand</em> and Merchandise out soon...</p>
+        <p class="sub-title">Album <em>Falling Sand</em> and Merchandise out now!</p>
         <a href="https://store.runningsons.com" class="button button-primary">Go to store</a>
     </div>
 </template>
