03af92e8c08dafcb58bd8411ad39e86692e68ab3
Braydon Jones <bradofrado@gmail.com>
2022-10-10 22:30:48 -0600

    Updated url links

--- global/components/LogoIcon.vue
+++ global/components/LogoIcon.vue
@@ -5,11 +5,11 @@ const urls = {
     instagram: "https://www.instagram.com/runningsons/",
     facebook: "https://www.facebook.com/braydon.jones.330/",
     github: "https://github.com/bradofrado/RunningSons",
-    // spotify: "https://github.com/bradofrado/RunningSons",
-    // apple: "https://music.apple.com/us/artist/1645664457",
-    // itunes: "https://music.apple.com/us/artist/1645664457",
-    // amazon: "https://github.com/bradofrado/RunningSons",
-    // pandora: "https://github.com/bradofrado/RunningSons",
+    spotify: "https://open.spotify.com/artist/4oUAnr1cEJYE4Zhl0TEQ8J",
+    apple: "https://music.apple.com/us/artist/running-sons/1645664457",
+    itunes: "https://music.apple.com/us/artist/running-sons/1645664457",
+    amazon: "https://music.amazon.com/artists/B0BFSWSL79/running-sons",
+    pandora: "https://www.pandora.com/artist/running-sons/AR4Z6mVblk2lf39",
     // youtube: "https://github.com/bradofrado/RunningSons",
 }
 
